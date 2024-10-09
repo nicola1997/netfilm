1238e4f7dc72857cc056d2002746f0f1dca7985d
ndipinto <nicola.dipinto@dxc.com>
2024-10-09 16:12:20 +0200

    fix icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import './NavBar.css'
+import icon from '../assets/icon.jpg'
 function NavBar() {
     return (
         <Navbar className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+                <Navbar.Brand >NETFILM <img src={icon} alt="img" style={{ width: '30px', height: '30px' }}/></Navbar.Brand>
                 <Navbar.Toggle/>
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
-                        Signed in as: <a href="#login">Mark Otto</a>
+                        Signed in as: Nicola Otto
                     </Navbar.Text>
                 </Navbar.Collapse>
             </Container>
