4fe0e82e9f02defc8146b3a37635d64e7945189a
ndipinto <nicola.dipinto@dxc.com>
2024-10-10 10:11:08 +0200

    add useSelector

--- src/components/Film.jsx
+++ src/components/Film.jsx
@@ -1,12 +1,24 @@
 import NavBar from "./NavBar.jsx";
 import CardFilm from "./CardFilm.jsx";
+import {useSelector} from "react-redux";
+import Container from "react-bootstrap/Container";
+import {Col, Row} from "react-bootstrap";
 
 function Film() {
+    const movies = useSelector((state) => state.movies);
+
     return (
         <div>
             <NavBar></NavBar>
-            <CardFilm></CardFilm>
-            FILM
+            <Container>
+                <Row>
+                    {movies.map((film, index) => (
+                        <Col key={index}>
+                            <CardFilm titolo={film.title} desc={film.descrizione} img={film.img} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     )
 }
