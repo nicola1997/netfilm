633916f559060980366c57989314d38727e50057
ndipinto <nicola.dipinto@dxc.com>
2024-10-11 13:04:17 +0200

    formatt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {useEffect} from 'react'
-import './App.css'
+import {useEffect} from 'react';
+import './App.css';
 import Navbar from "./components/NavBar.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {getMovie} from "./services/Movie.js";
@@ -9,12 +9,12 @@ import CardFilm from "./components/CardFilm.jsx";
 import {useDispatch, useSelector} from 'react-redux';
 import {setMovies} from "./redux/MovieSlice.js";
 import CarouselsFilm from "./components/CarouselsFilm.jsx";
-import './components/TextStyle.css'
-
+import './components/TextStyle.css';
 
 function App() {
     const dispatch = useDispatch();
     const movies = useSelector((state) => state.movies);
+
     const fetchMovies = async () => {
         try {
             const mv = await getMovie();
@@ -25,21 +25,25 @@ function App() {
     };
 
     useEffect(() => {
-        fetchMovies()
+        fetchMovies();
     }, []);
 
-    return (<div>
-        <Navbar></Navbar>
-        <CarouselsFilm></CarouselsFilm>
-        <h1 className='TextStyle'>Film che potrebbero piacerti </h1>
-        <Container>
-            <Row>
-                {movies?.map((film, index) => (<Col key={index}>
-                    <CardFilm titolo={film.title} desc={film.descrizione} img={film.img}/>
-                </Col>))}
-            </Row>
-        </Container>
-    </div>)
+    return (
+        <div>
+            <Navbar/>
+            <CarouselsFilm/>
+            <h1 className='TextStyle'>Film che potrebbero piacerti</h1>
+            <Container>
+                <Row>
+                    {movies.map((film, index) => (
+                        <Col key={index}>
+                            <CardFilm titolo={film.title} desc={film.descrizione} img={film.img}/>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
-export default App
+export default App;
