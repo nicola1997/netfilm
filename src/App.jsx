27758bf315c5d6acc746713e0d123c73b77d3133
ndipinto <nicola.dipinto@dxc.com>
2024-10-11 12:53:48 +0200

    fx redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import {useEffect} from 'react'
 import './App.css'
 import Navbar from "./components/NavBar.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,43 +10,36 @@ import {useDispatch, useSelector} from 'react-redux';
 import {setMovies} from "./redux/MovieSlice.js";
 import CarouselsFilm from "./components/CarouselsFilm.jsx";
 import './components/TextStyle.css'
-import EuroInput from "./components/EuroInput.jsx";
 
 
 function App() {
-    const [localMovies, setLocalMovies] = useState([]);
     const dispatch = useDispatch();
     const movies = useSelector((state) => state.movies);
-
-
     const fetchMovies = async () => {
-        const movies = await getMovie()
-        setLocalMovies(movies);
-        dispatch(setMovies(movies))
-    }
+        try {
+            const mv = await getMovie();
+            dispatch(setMovies(mv));
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+        }
+    };
 
     useEffect(() => {
         fetchMovies()
-
     }, []);
 
-
-    return (
-        <div>
-            <Navbar></Navbar>
-            <CarouselsFilm></CarouselsFilm>
-            <h1 className='TextStyle'>Film che potrebbero piacerti </h1>
-            <Container>
-                <Row>
-                    {localMovies.map((film, index) => (
-                        <Col key={index}>
-                            <CardFilm titolo={film.title} desc={film.descrizione} img={film.img}/>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        </div>
-    )
+    return (<div>
+        <Navbar></Navbar>
+        <CarouselsFilm></CarouselsFilm>
+        <h1 className='TextStyle'>Film che potrebbero piacerti </h1>
+        <Container>
+            <Row>
+                {movies?.map((film, index) => (<Col key={index}>
+                    <CardFilm titolo={film.title} desc={film.descrizione} img={film.img}/>
+                </Col>))}
+            </Row>
+        </Container>
+    </div>)
 }
 
 export default App
